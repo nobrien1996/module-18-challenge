a5462644559a212e1873e11880cd0db66743ff5b
Nick O'Brien <nickobriencomposer@gmail.com>
2024-11-11 16:03:19 -0500

    models written

---
+++ models/User.js
@@ -0,0 +1,45 @@
+const { Schema, model } = require('mongoos');
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            match: [/.+@+\..+/, 'Needs to match valid email'],
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought',
+            },
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User',
+            },
+        ],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false,
+    }
+);
+
+userSchema.virtual('friendCount').get(function () {
+    return this.friends.length;
+});
+
+const User = model('User', userSchema);
+
+
+module.exports = User;
